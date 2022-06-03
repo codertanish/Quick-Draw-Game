d81b5c063b243645b8c274ccbbc18ba988107244
codertanish <81834790+codertanish@users.noreply.github.com>
2022-06-03 18:59:54 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -12,4 +12,46 @@ sketch = quick_draw_data_set[random_number];
 document.getElementById("original_sketch").innerHTML = "Sketch To Be Drawn : " + sketch;
 
 
+function setup() {
+    canvas = createCanvas(280, 280);
+    canvas.center();
+    background("white");
+}
 
+
+function draw() {
+check_sketch();
+if(drawn_sketch == sketch) {
+    answer_holder = "set";
+    score++;
+    document.getElementById("score").innerHTML = "Score: " + score;
+}
+}
+
+function check_sketch() {
+    timer_counter++;
+    document.getElementById("time").innerHTML = "Timer: " + Math.ceil(timer_counter/75);
+    console.log(timer_counter);
+    if(timer_counter>800) {
+        timer_counter = 0;
+        timer_check = "completed";
+    }
+
+    if(timer_check == "completed" || answer_holder == "set") {
+        timer_check = "";
+        answer_holder = "";
+        updateCanvas();
+    }
+
+
+}
+
+function updateCanvas() {
+background("white");
+random_number = Math.floor(Math.random()*quick_draw_data_set.length + 1);
+sketch = quick_draw_data_set[random_number];
+console.log(sketch);
+document.getElementById("original_sketch").innerHTML = "Sketch To Be Drawn : " + sketch;
+
+
+}
